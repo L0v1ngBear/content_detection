a496bf41d673befec63e0ecaa27ff894f3f85fba
曹立峰 <caolf56397@hundsun.com>
2025-12-30 16:33:42 +0800

    增加mapper层

---
+++ springboot/src/main/java/org/clf/springboot/mapper/PictureMapper.java
@@ -0,0 +1,8 @@
+package org.clf.springboot.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.clf.springboot.entity.Picture;
+
+public interface PictureMapper extends BaseMapper<Picture> {
+
+}
